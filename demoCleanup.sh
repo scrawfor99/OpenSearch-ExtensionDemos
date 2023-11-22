3792157c7204451d8f3233c65401707384d98f83
Stephen Crawford <steecraw@amazon.com>
2023-11-22 14:52:58 -0500

    Update setup and cleanup
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

---
+++ demoCleanup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf OpenSearch
+
+rm -rf opensearch-sdk-java
+
+rm -rf opensearch-sdk-py
---
+++ demoSetup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone https://github.com/scrawfor99/OpenSearch.git
+
+git clone https://github.com/scrawfor99/opensearch-sdk-java.git
+
+git clone https://github.com/scrawfor99/opensearch-sdk-py.git
+
+
