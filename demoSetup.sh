ba22b079e8062e03f1cfa43dbafafd78d8239cf2
dblock <dblock@amazon.com>
2023-11-24 13:02:52 -0500

    Added k-nn demo.
    Signed-off-by: dblock <dblock@amazon.com>

--- demoSetup.sh
+++ demoSetup.sh
@@ -11,8 +11,14 @@ git clone https://github.com/opensearch-project/opensearch-sdk-java.git
 
 git clone https://github.com/opensearch-project/opensearch-sdk-py.git
 
+git clone https://github.com/opensearch-project/k-NN.git
+
 cd OpenSearch
 ./gradlew localDistro
+cd ..
+
+cd k-NN
+./gradlew assemble
 cd ..
 
 cd opensearch-sdk-py
